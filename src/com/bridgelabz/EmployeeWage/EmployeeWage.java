842c2e8dca264a05e78bbbca44a9cee21e9818a9
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-07 19:16:26 +0530

    Added EmployeeWage program

---
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.EmployeeWage;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to EmployeeWage Program");
+    }
+
+}
