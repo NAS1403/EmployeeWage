d394167e7be5fa4acb3f187b0e9eb702d6d2ca5b
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-17 14:08:51 +0530

    Modified: total wage for multiple companies

--- src/com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -2,12 +2,8 @@ package com.bridgelabz.EmployeeWage;
 public class EmployeeWage {
     static final int IS_FULL_TIME = 1;
     static final int IS_PART_TIME = 2;
-    static final int WAGE_PER_HOUR = 20;
-    static final int WORKING_DAY_PER_MONTH = 20;
-    static final int TOTAL_WORK_HOURS = 100;
 
-
-    static void calculateWage(){
+    static void calculateWage(String company, int wagePerHr,int daysPerMonth,int workHrPerMonth){
 
         int empHrs;
         int totalHours=0;
@@ -15,7 +11,7 @@ public class EmployeeWage {
         int day=0;
         int totalWage=0;
 
-        while((totalHours<TOTAL_WORK_HOURS) && (day<WORKING_DAY_PER_MONTH)) {
+        while((totalHours<workHrPerMonth) && (day<daysPerMonth)) {
 
             int attendance = (int)(Math.floor(Math.random()*10))%3;
             switch (attendance) {
@@ -30,19 +26,21 @@ public class EmployeeWage {
                     break;
             }
             totalHours+=empHrs;
-            dailyWage = WAGE_PER_HOUR*empHrs;
+            dailyWage = wagePerHr*empHrs;
             totalWage+=dailyWage;
             day++;
             System.out.println("Day "+day+" Emp work hour "+empHrs+ " wage is "+dailyWage);
         }
         System.out.println("Total work hours is : "+totalHours);
         System.out.println("Total work days are: " +day);
-        System.out.println("Total wage is: "+totalWage);
+        //System.out.println("Total wage is: "+totalWage);
+        System.out.println("Total Wage of company "+company+ " is "+ totalWage);
 
     }
 
     public static void main(String[] args) {
-        calculateWage();
+        calculateWage("BEL",10,20,100);
+        calculateWage("SitusAMC",20,22,90);
     }
 
 }
